0249e757bc3f7c025f8fd2cf82d02afcd3d9ece6
kwenn <edu.gacelosjanina@gmail.com>
2025-06-04 12:33:15 +0800

    ongoing email

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -25,7 +25,53 @@ class MainController extends CI_Controller{
 
         $insert_contact = new Main_Model;
         $insert_contact->insert_data($contact_data,'tbl_contact', $log_data);
+        
+        
+        
 
+    }
+
+    public function contact_send_email($contact_data){
+            // PHPMailer object
+            $mail = $this->phpmailer_lib->load();
+    
+            // SMTP configuration
+            $mail->isSMTP();
+            $mail->Host     = 'smtp.example.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'user@example.com';
+            $mail->Password = '********';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port     = 465;
+    
+            $mail->setFrom('info@example.com', 'CodexWorld');
+            $mail->addReplyTo('info@example.com', 'CodexWorld');
+    
+            // Add a recipient
+            $mail->addAddress('vocoho1899@cigidea.com');
+    
+            // Add cc or bcc 
+            $mail->addCC('cc@example.com');
+            $mail->addBCC('bcc@example.com');
+    
+            // Email subject
+            $mail->Subject = 'Send Email via SMTP using PHPMailer in CodeIgniter';
+    
+            // Set email format to HTML
+            $mail->isHTML(true);
+    
+            // Email body content
+            $mailContent = "<h1>Send HTML Email using SMTP in CodeIgniter</h1>
+                <p>This is a test email sending using SMTP mail server with PHPMailer.</p>";
+            $mail->Body = $mailContent;
+    
+            // Send email
+            if(!$mail->send()){
+                echo 'Message could not be sent.';
+                echo 'Mailer Error: ' . $mail->ErrorInfo;
+            }else{
+                echo 'Message has been sent';
+            }
     }
 }
 ?>
