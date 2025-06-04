df7ebcae79e8420af2acf0b7e9cc5a3f75139810
Kwenn <edu.gacelos@gmail.com>
2025-06-04 19:52:57 +0800

    sql

---
+++ sql/tbls.sql
@@ -0,0 +1,77 @@
+/*
+SQLyog Community v13.3.0 (64 bit)
+MySQL - 10.4.27-MariaDB : Database - db_resume
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_resume` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `db_resume`;
+
+/*Table structure for table `tbl_contact` */
+
+DROP TABLE IF EXISTS `tbl_contact`;
+
+CREATE TABLE `tbl_contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_name` varchar(255) DEFAULT NULL,
+  `contact_email` varchar(255) DEFAULT NULL,
+  `contact_subject` varchar(255) DEFAULT NULL,
+  `contact_message` text DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `status` int(11) DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tbl_contact` */
+
+insert  into `tbl_contact`(`id`,`contact_name`,`contact_email`,`contact_subject`,`contact_message`,`created_at`,`status`) values 
+(1,'test','test@email.com','test','test','2025-06-04 17:52:23',1),
+(2,'test','test@email.com','test','test','2025-06-04 18:59:29',1),
+(3,'test','test@email.com','test','test','2025-06-04 18:59:35',1);
+
+/*Table structure for table `tbl_files` */
+
+DROP TABLE IF EXISTS `tbl_files`;
+
+CREATE TABLE `tbl_files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(255) DEFAULT NULL,
+  `file_path` varchar(255) DEFAULT NULL,
+  `file_type` varchar(50) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tbl_files` */
+
+insert  into `tbl_files`(`id`,`file_name`,`file_path`,`file_type`,`created_at`) values 
+(1,'test.jpg','uploads/test.jpg','image/jpeg','2025-06-04 19:44:02');
+
+/*Table structure for table `tbl_personal_info` */
+
+DROP TABLE IF EXISTS `tbl_personal_info`;
+
+CREATE TABLE `tbl_personal_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `introduction` text NOT NULL,
+  `profile_picture_id` int(11) NOT NULL,
+  `professional_title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tbl_personal_info` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
